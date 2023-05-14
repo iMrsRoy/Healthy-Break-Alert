9680734b6d5509d237e53d8fbeef974b0d321a26
Rajarshi Roy ♠ <10099786+iMrsRoy@users.noreply.github.com>
2023-05-14 11:51:15 -0400

    moved the folders

--- app.py
+++ app.py
@@ -1,8 +1,24 @@
 """Greeting Flask app."""
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, request;
+from flask import render_template, redirect, url_for; 
 
+
+app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, world!'
+    return render_template('index.html')
+
+
+
+@app.route('/login')
+def login():
+    return redirect(url_for('home'))
+    ''' , methods=['POST']
+    email = request.form['email']
+    password = request.form['password']
+    # TODO: validate username and password'''
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host= '0.0.0.0', port = 5000)
