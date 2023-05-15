310a25c8ac509b0454eeb5f27b535ce9589e07e6
Rajarshi Roy ♠ <10099786+iMrsRoy@users.noreply.github.com>
2023-05-15 10:54:28 -0400

    login pg is working  now added base.html

--- app.py
+++ app.py
@@ -1,26 +1,68 @@
 """Greeting Flask app."""
-from flask import Flask, request;
-from flask import render_template, redirect, url_for, routes
+from flask import Flask, render_template, redirect, request, session, flash, url_for
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-db = SQLAlchemy(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
-app.config['SECRET_KEY'] = 'thisisasecretkey'
 
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+#route for your registration page
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        user = User.query.filter_by(username=username).first()
+        if user:
+            return redirect(url_for('login'))
+
+        new_user = User(username=username, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+
+        return redirect(url_for('index'))
+
+    return render_template('register.html')
+
+#route to login page
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        user = User.query.filter_by(username=username, password=password).first()
+        if user:
+            session['username'] = username
+
+            return redirect(url_for('index'))
+
+        return redirect(url_for('login'))
+
+    return render_template('login.html')
 
 @app.route('/')
-def home():
-    return render_template('index.html')
+def index():
+    # Check if the user is logged in
+    if 'username' in session:
+        return 'Welcome, ' + session['username'] + '!'
 
-@app.route('/login')
-def login():
-    return redirect(url_for('home'))
-    ''' , methods=['POST']
-    email = request.form['email']
-    password = request.form['password']
-    # TODO: validate username and password'''
+    return redirect(url_for('login'))
 
+
+
+#debug off
 if __name__ == '__main__':
     app.debug = True
     app.run(host= '0.0.0.0', port = 5000)
+
